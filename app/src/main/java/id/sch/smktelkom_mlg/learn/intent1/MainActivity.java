2aedfd563a483f6426094fcded85ad27ed0822d2
TrischaNL <l.trischa@gmail.com>
2016-10-06 06:05:36 -0700

    startActivity Part 1

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/MainActivity.java
@@ -1,13 +1,27 @@
 package id.sch.smktelkom_mlg.learn.intent1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btHitung;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btHitung = (Button) findViewById(R.id.buttonHitung);
+        btHitung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MainActivity.this, HasilActivity.class));
+            }
+        });
     }
 }
